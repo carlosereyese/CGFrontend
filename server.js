ad5125878748c111f79a0672bfa8cd75567ab967
carlosereyese <carlosereyese@gmail.com>
2024-01-17 19:59:01 +0100

    fixed paths

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/your-angular-app-name'));
+app.use(express.static(__dirname + '/dist/front-end'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/your-angular-app-name/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/front-end/index.html'));
 });
 
 const port = process.env.PORT || 3000;
